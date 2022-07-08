420d6bf44b301680873c2d8ef3e4065721e6e071
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-07-08 15:14:16 +0430

    Simple channels

---
+++ simple-channels/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping, pong chan string) {
+	for {
+		s := <-ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+
+func main() {
+	// create two channels
+	ping := make(chan string)
+	pong := make(chan string)
+	// 
+	go shout(ping, pong)
+
+	fmt.Println("Type something and press enter(enter q to quite)")
+
+	for {
+		fmt.Print("->")
+
+		// get user input
+
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+		if userInput == strings.ToLower("q") {
+			break
+		}
+
+		ping <- userInput
+
+		// wait for a response
+		response := <-pong
+		fmt.Println("Response: ", response)
+
+	}
+
+	fmt.Println("All done closing channels")
+	close(ping)
+	close(pong)
+
+}
