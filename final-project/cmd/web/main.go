122672af1118bf1cd68edc1029af565c2f0c1629
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-07-08 18:32:35 +0430

    databse config

---
+++ final-project/cmd/web/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+func main() {
+	// connect to database
+
+	// create sessions
+
+	// create some channels
+
+	// create waitgroup
+
+	// set up the application config
+
+	// sending email
+
+	// listen for web connections
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("can't connect to database")
+	}
+	return conn
+}
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgress not yet ready...")
+		} else {
+			log.Println("connected to database!")
+			return connection
+		}
+		if counts > 10 {
+			return nil
+		}
+		log.Print("Backing off for 1 seconds")
+		time.Sleep(time.Second * 10)
+	}
+
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
